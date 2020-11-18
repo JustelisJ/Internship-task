269e5a0a00f99b9be317fa9c237a72c4bb41a754
JustelisJ <j.jancys@gmail.com>
2020-11-18 11:44:34 +0200

    Initial commit

---
+++ src/test/java/blaster/help/form/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package blaster.help.form.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

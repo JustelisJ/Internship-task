e0fe01764478cf31892c4a8df6d9ef18d1e32871
JustelisJ <j.jancys@gmail.com>
2020-11-26 14:21:33 +0200

    Mock department list GuestForm.html takes department list, instead of having it static

--- src/main/java/blaster/help/form/controller/FormController.java
+++ src/main/java/blaster/help/form/controller/FormController.java
@@ -1,5 +1,6 @@
 package blaster.help.form.controller;
 
+import blaster.help.form.domain.Department;
 import blaster.help.form.domain.Employee;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @Log4j2
 public class FormController {
@@ -15,6 +19,7 @@ public class FormController {
     @GetMapping({"", "/"})
     public String openForm(Model model) {
         model.addAttribute("employee", new Employee());
+        model.addAttribute("departments", mockDepartmentList());
 
         return "form/GuestForm";
     }
@@ -26,6 +31,21 @@ public class FormController {
         log.info("Employee email: " + employee.getEmail());
         log.info("Employee department: " + employee.getDepartment().getName());
         return "redirect: ";
+    }
+
+    private List<Department> mockDepartmentList() {
+        List<Department> departments = new ArrayList<>();
+        departments.add(new Department("TeleSoftas Kaunas"));
+        departments.add(new Department("TeleSoftas Vilnius"));
+        departments.add(new Department("TeleSoftas Klaipėda"));
+        departments.add(new Department("Adeo Web Kaunas"));
+        departments.add(new Department("Adeo Web Vilnius"));
+        departments.add(new Department("SneakyBox Kaunas"));
+        departments.add(new Department("SneakyBox Vilnius"));
+        departments.add(new Department("MobOn"));
+        departments.add(new Department("OKTA"));
+        departments.add(new Department("Agmis"));
+        return departments;
     }
 
 }

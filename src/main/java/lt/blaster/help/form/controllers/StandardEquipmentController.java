f04e8bd5940c67465f16025f734b0d3984aec183
JustelisJ <j.jancys@gmail.com>
2020-12-01 14:36:26 +0200

    Added Validation dependency EquipmentOrder list is checked if it is empty standardEquipment form shows if none of the equipments are chosen

--- src/main/java/lt/blaster/help/form/controllers/StandardEquipmentController.java
+++ src/main/java/lt/blaster/help/form/controllers/StandardEquipmentController.java
@@ -5,25 +5,38 @@ import lt.blaster.help.form.domain.EquipmentOrder;
 import lt.blaster.help.form.domain.StandardEquipment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 
 @Controller
 @Log4j2
 public class StandardEquipmentController {
 
+    private static final String STANDARD_EQUIPMENT_FORM = "form/standardEquipment";
+
     @GetMapping("/standardEquipment")
     public String getStandardEquipmentForm(Model model) {
         model.addAttribute("today", LocalDate.now());
         model.addAttribute("order", new EquipmentOrder());
         model.addAttribute("allEquipment", StandardEquipment.mockStandardEquipment());
-        return "form/standardEquipment";
+        return STANDARD_EQUIPMENT_FORM;
     }
 
     @PostMapping("/standardEquipment")
-    public String postStandardEquipmentOrder(EquipmentOrder order) {
+    public String postStandardEquipmentOrder(@Valid @ModelAttribute("order") EquipmentOrder order,
+                                             BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(objectError -> log.error(objectError.toString()));
+            model.addAttribute("today", LocalDate.now());
+            model.addAttribute("allEquipment", StandardEquipment.mockStandardEquipment());
+            return STANDARD_EQUIPMENT_FORM;
+        }
+
         log.error(order.toString());
         return "redirect:/standardEquipment";
     }
--- src/main/java/lt/blaster/help/form/domain/EquipmentOrder.java
+++ src/main/java/lt/blaster/help/form/domain/EquipmentOrder.java
@@ -3,6 +3,7 @@ package lt.blaster.help.form.domain;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 @Data
 public class EquipmentOrder {
 
+    @NotEmpty(message = "Need to select at least one equipment")
     private List<StandardEquipment> equipments;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate deadline;
